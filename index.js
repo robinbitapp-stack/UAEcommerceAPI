db6ca9cf384f7868dc9e6b26f920286779e59ffb
robinbitapp-stack <robinbitapp@gmail.com>
2025-10-26 21:24:41 +0530

    newupdate

--- index.js
+++ index.js
@@ -678,7 +678,7 @@ async function syncCategoryProducts(categoryId) {
         consumer_key: 'ck_bb500a1fb70b1094d43fd85296ad10c5dada160b',
         consumer_secret: 'cs_b7232701e74d5e22fe79c70b312e36acb4d8757a'
       },
-      timeout: 10000,
+      timeout: 30000,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
         'Accept': 'application/json',
@@ -1120,9 +1120,23 @@ app.get('/getCategoriesProductWithSortFilterOld', async (req, res) => {
     if (on_sale === 'true') wooCommerceParams.on_sale = true;
     if (stock_status) wooCommerceParams.stock_status = stock_status;
 
-    const response = await apiAxios.get('products', {
+    // const response = await apiAxios.get('products', {
+    //   params: wooCommerceParams,
+    //   timeout: 10000
+    // });
+
+    const response = await axios.get('https://updateavenues.com/wp-json/wc/v3/products', {
       params: wooCommerceParams,
-      timeout: 10000
+        timeout: 10000,
+        headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Accept': 'application/json',
+        'Referer': 'https://updateavenues.com/',
+        'Origin': 'https://updateavenues.com'
+      },
+      httpsAgent: new (require('https').Agent)({  
+        rejectUnauthorized: false
+      })
     });
 
     let products = response.data;
@@ -1218,10 +1232,24 @@ app.get('/getCategoriesProductWithSortFilter', async (req, res) => {
       if (on_sale === 'true') wooCommerceParams.on_sale = true;
       if (stock_status) wooCommerceParams.stock_status = stock_status;
 
-      const response = await apiAxios.get('products', {
-        params: wooCommerceParams,
-        timeout: 10000
-      });
+      // const response = await apiAxios.get('products', {
+      //   params: wooCommerceParams,
+      //   timeout: 10000
+      // });
+
+      const response = await axios.get('https://updateavenues.com/wp-json/wc/v3/products', {
+      params: wooCommerceParams,
+        timeout: 10000,
+        headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+        'Accept': 'application/json',
+        'Referer': 'https://updateavenues.com/',
+        'Origin': 'https://updateavenues.com'
+      },
+      httpsAgent: new (require('https').Agent)({  
+        rejectUnauthorized: false
+      })
+    });
 
       let products = response.data;
 
